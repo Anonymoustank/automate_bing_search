8dfec97a988fa6d92b0bfa45273143622d3038ad
Anonymoustank <54193508+Anonymoustank@users.noreply.github.com>
2020-08-17 16:10:44 -0600

    Add files via upload

---
+++ Mobile_Search.py
@@ -0,0 +1,10 @@
+import os
+import time
+number = 100
+
+for i in range(5):
+    for i in range(4):
+        os.system('/data/data/com.termux/files/usr/bin/xdg-open("http://www.bing.com/search?q=%s")' % number)
+        number += 1
+        time.sleep(1.5)
+    time.sleep(5)
