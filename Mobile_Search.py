74a2866a0b2e8dcafc35a4fee740388e77710766
Anonymoustank <54193508+Anonymoustank@users.noreply.github.com>
2020-08-17 16:11:48 -0600

    Update Mobile_Search.py

--- Mobile_Search.py
+++ Mobile_Search.py
@@ -4,7 +4,7 @@ number = 100
 
 for i in range(5):
     for i in range(4):
-        os.system('/data/data/com.termux/files/usr/bin/xdg-open("http://www.bing.com/search?q=%s")' % number)
+        os.system('/data/data/com.termux/files/usr/bin/xdg-open "http://www.bing.com/search?q=%s"' % number)
         number += 1
         time.sleep(1.5)
     time.sleep(5)
