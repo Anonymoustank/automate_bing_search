538dde4fcfb3bfed8e3a4998613ee684b8177276
Anonymoustank <pkadekodi@gmail.com>
2020-08-17 20:35:35 -0600

    Updated files

--- Main.py
+++ Main.py
@@ -1,11 +1,13 @@
 import os
 import time
 
-number = 0
+number = 1
 for i in range(5):
     for i in range(7):
         print(os.system('powershell C:/Users/Pranav/Downloads/SearchInternet/SearchInternet/PS_SearchNet.ps1 -SearchFor "f%s" -Use "Bing"' % number))
         number += 1
         time.sleep(1.5)
+    if number == 34:
+        break
     time.sleep(5)
     os.system("powershell Stop-Process -Name msedge")
--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Tired of having to actually search stuff up on Bing to get rewards points? Look 
 Prerequisites (Android): Python 3, Termux (or another Linux-like terminal)
 
 Prerequisites (Windows 10): Python 3, PS_SearchNet.ps1 (included in this repo)
+
+Source of the powershell file: https://gallery.technet.microsoft.com/Use-PowerShell-To-Search-db915a65
