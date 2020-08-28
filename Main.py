86df113f235f2a6982d0aaa3b6da1937aa40d1d0
Anonymoustank <pkadekodi@gmail.com>
2020-08-28 14:44:11 -0600

    You can pass in the amount of times you want the program to perform a Bing Search

--- Main.py
+++ Main.py
@@ -1,13 +1,28 @@
 import os
 import time
-
+import sys
+error = 0
 number = 1
-for i in range(5):
-    for i in range(7):
-        print(os.system('./PS_SearchNet.ps1 -SearchFor "f%s" -Use "Bing"' % number))
-        number += 1
+loop_num = 34
+
+if len(sys.argv) > 1:
+    try:
+        loop_num = int(sys.argv[1])
+    except:
+        print("Make sure the number passed is a positive integer")
+        
+if loop_num <= 0:
+    print("Make sure the number passed is a positive integer")
+
+for i in range(loop_num):
+    if os.system('powershell C:/Users/Pranav/Desktop/Search/PS_SearchNet.ps1 -SearchFor "f%s" -Use "Bing"' % number) != 0:
+        error += 1
+    number += 1
+    if number % 7 == 0:
+        time.sleep(3)
+        os.system("powershell Stop-Process -Name msedge")
+    else:
         time.sleep(1.5)
-    if number == 34:
-        break
-    time.sleep(5)
-    os.system("powershell Stop-Process -Name msedge")
+
+print("This program ran into %s errors" % error)
+time.sleep(2)
