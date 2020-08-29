db50019777548fdc8f0d2311a0ed050031f68fd1
Anonymoustank <pkadekodi@gmail.com>
2020-08-28 19:21:16 -0600

    Exits after running into an exception

--- Main.py
+++ Main.py
@@ -10,9 +10,11 @@ if len(sys.argv) > 1:
         loop_num = int(sys.argv[1])
     except:
         print("Make sure the number passed is a positive integer")
+        exit(1)
         
 if loop_num <= 0:
     print("Make sure the number passed is a positive integer")
+    exit(1)
 
 for i in range(loop_num):
     if os.system('powershell C:/Users/Pranav/Desktop/Search/PS_SearchNet.ps1 -SearchFor "f%s" -Use "Bing"' % number) != 0:
