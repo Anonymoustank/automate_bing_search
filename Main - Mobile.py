27232432091524d6a901f294f2fbf8111dd9ea93
Anonymoustank <pkadekodi@gmail.com>
2020-08-31 09:53:52 -0600

    You can pass in the amount of times you want the program to do a bing search

--- Main - Mobile.py
+++ Main - Mobile.py
@@ -1,10 +1,30 @@
 import os
 import time
+import sys
+error = 0
 number = 100
+loop_num = 20
 
-for i in range(5):
-    for i in range(4):
-        os.system('/data/data/com.termux/files/usr/bin/xdg-open "http://www.bing.com/search?q=%s"' % number)
-        number += 1
+if len(sys.argv) > 1:
+    try:
+        loop_num = int(sys.argv[1])
+    except:
+        print("Make sure the number passed is a positive integer")
+        exit(1)
+
+if loop_num <= 0:
+    print("Make sure the number passed is a positive integer")
+    exit(1)
+
+for i in range(loop_num):
+    if os.system('xdg-open "http://www.bing.com/search?q=f%s"' % number) != 0:
+        error += 1
+    number += 1
+    if number % 5 == 0:
+        time.sleep(5)
+    else:
         time.sleep(1.5)
-    time.sleep(5)
+
+print("This program ran into %s errors" % error)
+time.sleep(2)
+
